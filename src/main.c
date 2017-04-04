2a4ac6741f14a3a3008e38e71a57e0edea03116b
Ben Sheffield <bshefb@gmail.com>
2017-04-04 02:11:44 +0100

    Added command line parsing to main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,45 @@
 #include "includes/error.h"
 
-int main() {
+void print_usage() {
+	printf("usage: fur filename <flags>\n");
+	exit(1);
+}
+
+int main(int argc, char *argv[]) {
+	char *in_file = argv[1];
+	char *out_file = "";
+	bool tokens = false;
+	bool ircode = false;
+
+	// Check for no/incorrect input file
+	if (argc < 2 || argv[0][0] == '-') print_usage();
+	
+	// Check input file has .fur extension
+	int in_len = strlen(in_file);
+	if(in_len < 4) print_usage();
+	if(strcmp(in_file + in_len - 4, ".fur") != 0) print_usage();
+	
+	// Create default out name
+	out_file = malloc(in_len - 3 * sizeof(char));
+	memcpy(out_file, in_file, (in_len - 4) * sizeof(char));
+	out_file[in_len - 3] = '\0';
+	
+	// Parse tokens
+	for (int i = 2; i < argc; i++) {
+		if(strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "-output") == 0) {
+			if(i == argc - 1) print_usage();
+			i++;
+			if(argv[i][0] == '-') print_usage();
+			out_file = argv[i];
+			i++;
+		} else {
+			tokens = tokens || strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "-tokens") == 0;
+			ircode = ircode || strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "-ircode") == 0;
+		}
+	}
+
+	printf("In: %s\nOut: %s\nTokens: %d\nIrcode: %d\n", in_file, out_file, tokens, ircode);
+	
 	char *src = "proc add :: int a, int b -> int {\n\treturn a + c\n}";
 
 	error(
