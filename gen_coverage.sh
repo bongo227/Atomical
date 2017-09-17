ea02a4644a5bfa69f5e58fc2124fd0f0f55d4ac9
Ben Sheffield <bshefb@gmail.com>
2017-09-17 17:19:41 +0100

    Switched to gcc-7 and created a scripted to generate coverage files

---
+++ gen_coverage.sh
@@ -0,0 +1,16 @@
+cd build &&
+# Remove build files
+rm -rf CMakeFiles/ &&
+rm -rf coverage/ &&
+rm -f atomical-test cmake_install.cmake CMakeCache.txt libatomical.a Makefile &&
+# Make and run
+cmake . && 
+make &&
+./atomical-test &&
+# Generate coverage files
+mkdir coverage &&
+find . -name "test.cpp.gcda" -exec mv {} ./coverage \; &&
+find . -name "test.cpp.gcno" -exec mv {} ./coverage \; &&
+lcov --directory ./coverage --capture --output-file ./coverage/coverage.info &&
+lcov --remove ./coverage/coverage.info '/usr/*' '*_test.cpp' '*doctest.h' --output-file ./coverage/coverage.info &&
+lcov --list ./coverage/coverage.info
